185aec27ba4b6705d450093c2e88e38ca88ae5c2
Don Maxito <maxharz@gmail.com>
2018-04-12 16:28:25 -0600

    code style, clean up

--- keyboard-handler/keyboard-handler.js
+++ keyboard-handler/keyboard-handler.js
@@ -1,13 +1,16 @@
 export default class KeyboardHandler {
     // Set individual listener. Only one can be active at a time.
     static setGlobalListener(listener) {
-        if (KeyboardHandler.listener !== undefined) KeyboardHandler.removeGlobalListener();
+        if (KeyboardHandler.listener !== undefined) {
+            KeyboardHandler.removeGlobalListener();
+        }
+
         KeyboardHandler.listener = listener;
-        window.addEventListener('keypress', KeyboardHandler.listener);
+        self.addEventListener('keypress', KeyboardHandler.listener);
     }
 
     static removeGlobalListener() {
-        window.removeEventListener('keypress', KeyboardHandler.listener);
+        self.removeEventListener('keypress', KeyboardHandler.listener);
     }
 
     static setDefaultTarget(target) {
@@ -16,11 +19,13 @@ export default class KeyboardHandler {
 
     // For use with setDefaultTarget
     static _listen(e) {
-        const input = e.key;
         if (e.keyCode === 13) return; // enter key
+
         const activeElement = document.activeElement && document.activeElement.className;
         const isInInput = activeElement === 'input' || activeElement === 'textarea';
+
         if (isInInput) return;  // We are interested in the case were we're NOT in an input yet
+
         e.stopPropagation();
         this.focus();
     }
